6725193d09e0e9bd6683b6c2bb13c055fadf3914
stefankovac10 <stefankovac42@gmail.com>
2020-05-21 22:54:37 +0200

    Car service created

---
+++ car_service/src/main/java/xws/tim16/car_service/model/Comment.java
@@ -0,0 +1,10 @@
+package xws.tim16.car_service.model;
+
+public class Comment {
+   private String text;
+   private boolean approved;
+
+   private User user;
+   private Car car;
+
+}
---
+++ car_service/src/main/java/xws/tim16/car_service/model/Mark.java
@@ -0,0 +1,6 @@
+package xws.tim16.car_service.model;
+
+public class Mark {
+   private String name;
+
+}
