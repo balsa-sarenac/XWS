111d18c08768700149f9f3453f58eecd00046029
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-05-21 17:42:12 +0200

    Created microservice applications

---
+++ ads_service/src/test/java/xws/tim16/ads_service/AdsServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xws.tim16.ads_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ request_service/src/test/java/xws/tim16/request_service/RequestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xws.tim16.request_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RequestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
