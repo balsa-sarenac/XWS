f9b95272fac6e869ff9e04f3af2deae0a5a8377f
meelosch <57445151+meelosch@users.noreply.github.com>
2020-05-21 23:09:12 +0200

    added required model classes

---
+++ request_service/src/main/java/xws/tim16/request_service/model/Ad.java
@@ -0,0 +1,17 @@
+package xws.tim16.request_service.model;
+
+
+import java.util.Set;
+
+public class Ad {
+   private boolean cdwAvailable;
+   private String pickUpPlace;
+   private String pickUpDate;
+   private String returnDate;
+   private double allowedKilometrage;
+
+   private Set<RentRequest> request;
+   private Car car;
+   private PriceList priceList;
+
+}
---
+++ request_service/src/main/java/xws/tim16/request_service/model/User.java
@@ -0,0 +1,16 @@
+package xws.tim16.request_service.model;
+
+import java.util.Set;
+
+public class User {
+   private String username;
+   private String password;
+   private String firstName;
+   private String lastName;
+   private boolean isEnabled;
+
+   private Set<Car> cars;
+   private Set<Ad> ads;
+
+
+}
