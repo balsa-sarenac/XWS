2ac30d544d7e9beb035d02f98a7daf2846c63888
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-05-27 18:11:19 +0200

    Configured eureka and zuul

--- eureka/src/main/java/xws/team16/eureka/EurekaApplication.java
+++ eureka/src/main/java/xws/team16/eureka/EurekaApplication.java
@@ -1,34 +1,15 @@
-<<<<<<< HEAD
-<<<<<<< HEAD:eureka/src/main/java/xws/team16/eureka/EurekaApplication.java
 package xws.team16.eureka;
-=======
-package xws.team16.adservice;
->>>>>>> a2f400b1c749d510883ae8c376f32c1ffc18c3b9:ad-service/src/main/java/xws/team16/adservice/AdServiceApplication.java
-=======
-package xws.team16.eureka;
->>>>>>> a2f400b1c749d510883ae8c376f32c1ffc18c3b9
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
-<<<<<<< HEAD
-<<<<<<< HEAD:eureka/src/main/java/xws/team16/eureka/EurekaApplication.java
-=======
->>>>>>> a2f400b1c749d510883ae8c376f32c1ffc18c3b9
+@EnableEurekaServer
 public class EurekaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EurekaApplication.class, args);
-<<<<<<< HEAD
-=======
-public class AdServiceApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(AdServiceApplication.class, args);
->>>>>>> a2f400b1c749d510883ae8c376f32c1ffc18c3b9:ad-service/src/main/java/xws/team16/adservice/AdServiceApplication.java
-=======
->>>>>>> a2f400b1c749d510883ae8c376f32c1ffc18c3b9
 	}
 
 }
