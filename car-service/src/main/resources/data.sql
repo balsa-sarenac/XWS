0130269ee39da0cb85ec87c11fe8c29401a35c80
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-05-30 15:41:25 +0200

    Created data.sql

---
+++ car-service/src/main/resources/data.sql
@@ -0,0 +1,6 @@
+insert into USER_TABLE (first_name, last_name, username, password, is_enabled) values ('balsa', 'sarenac', 'bax', 'asdf', true);
+insert into MARK (name ) values ('audi');
+insert into MODEL (name, mark_id) values ('q7', 1);
+insert into FUEL (type) values ('benzin');
+insert into CAR_CLASS (name ) values ('city');
+insert into GEARBOX (type) values ('automatic');
