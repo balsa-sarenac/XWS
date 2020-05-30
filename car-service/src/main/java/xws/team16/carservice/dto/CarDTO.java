ec7fc1259f053db881de68164b423166f769e291
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-05-30 15:40:37 +0200

    Updated CarDTO

--- car-service/src/main/java/xws/team16/carservice/dto/CarDTO.java
+++ car-service/src/main/java/xws/team16/carservice/dto/CarDTO.java
@@ -1,5 +1,17 @@
 package xws.team16.carservice.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter
+@NoArgsConstructor
 public class CarDTO {
-    private String model;
+    private Long modelId;
+    private Long markId;
+    private Long fuelId;
+    private Long carClassId;
+    private Long gearboxId;
+    private Long userId;
 }
