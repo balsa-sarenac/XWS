5885e15a09d255a2ba56dce2024bbc10c71bf83b
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-05-30 15:41:15 +0200

    Added missing annotation

--- car-service/src/main/java/xws/team16/carservice/CarServiceApplication.java
+++ car-service/src/main/java/xws/team16/carservice/CarServiceApplication.java
@@ -2,11 +2,13 @@ package xws.team16.carservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@EnableEurekaClient
 //@EnableFeignClients
 public class CarServiceApplication {
 
