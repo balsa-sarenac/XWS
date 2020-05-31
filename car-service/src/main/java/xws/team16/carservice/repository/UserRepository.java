a4e0adfd0f6b22466b028155393f681701e80f55
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-05-31 20:22:47 +0200

    Created needed repositories

--- car-service/src/main/java/xws/team16/carservice/repository/UserRepository.java
+++ car-service/src/main/java/xws/team16/carservice/repository/UserRepository.java
@@ -4,6 +4,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import xws.team16.carservice.model.User;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+
+//    User findByUsername(String bax);
+    Optional<User> findByUsername(String username);
 }
