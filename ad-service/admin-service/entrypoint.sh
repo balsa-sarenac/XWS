490fbd9096dd1aef4606e2fd91977f750112a613
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-06-01 13:25:16 +0200

    Merging master into feature-1.0-security

---
+++ ad-service/admin-service/entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+java -jar admin-0.0.1.jar
---
+++ ad-service/admin-service/src/test/java/xws/team16/adminservice/AdminServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package xws.team16.adminservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
