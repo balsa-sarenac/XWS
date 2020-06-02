a0a576bf8492ca077c3f2757a08d81c14b298d68
Stefan Kovač <stefankovac42@gmail.com>
2020-06-02 22:55:00 +0200

    Added h2 database

--- security-service/src/main/java/xws/team16/securityservice/SecurityServiceApplication.java
+++ security-service/src/main/java/xws/team16/securityservice/SecurityServiceApplication.java
@@ -2,8 +2,14 @@ package xws.team16.securityservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
+@RestController
+@EnableEurekaClient
 public class SecurityServiceApplication {
 
 	public static void main(String[] args) {
