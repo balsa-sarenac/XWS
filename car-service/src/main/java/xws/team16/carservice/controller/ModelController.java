5e6cc2a56db76becdf667f974a0cd9054196446c
meelosch <57445151+meelosch@users.noreply.github.com>
2020-06-04 00:07:19 +0200

    made ModelController class

---
+++ car-service/src/main/java/xws/team16/carservice/controller/ModelController.java
@@ -0,0 +1,23 @@
+package xws.team16.carservice.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import xws.team16.carservice.service.ModelService;
+
+@Slf4j
+@RestController
+@RequestMapping(value = "/model")
+public class ModelController {
+
+    private ModelService modelService;
+
+    @Autowired
+    public ModelController(ModelService modelService) {
+        this.modelService = modelService;
+    }
+
+
+}
