50fc54210810937bc788bb1fedfe48db9b56b370
Stefan Kovač <stefankovac42@gmail.com>
2020-06-05 18:45:47 +0200

    Implemented logic for creating new occupied

---
+++ car-service/src/main/java/xws/team16/carservice/controller/OccupiedController.java
@@ -0,0 +1,35 @@
+package xws.team16.carservice.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import xws.team16.carservice.dto.OccupiedDTO;
+import xws.team16.carservice.service.OccupiedService;
+
+@RestController @Slf4j
+@RequestMapping(value = "/occupied")
+public class OccupiedController {
+    private OccupiedService occupiedService;
+
+    @Autowired
+    public OccupiedController(OccupiedService occupiedService) {
+        this.occupiedService = occupiedService;
+    }
+
+    /**
+     * Creates Occupied for specific car
+     */
+    @PostMapping
+    public ResponseEntity<?> addNew(@RequestBody OccupiedDTO occupiedDTO) {
+        log.info("Occupied controller - new occupied");
+        return this.occupiedService.newOccupied(occupiedDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> addNew(@PathVariable Long id ) {
+        log.info("Occupied controller - get occupied");
+        return this.occupiedService.getOccupied(id);
+    }
+
+}
