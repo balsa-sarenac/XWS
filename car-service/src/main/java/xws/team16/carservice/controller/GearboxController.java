f6a25ba08ab47620483b3ca2d1e8c7c5c0fcc6a9
meelosch <57445151+meelosch@users.noreply.github.com>
2020-06-05 14:01:52 +0200

    implemented method for getting Gearbox by its id

--- car-service/src/main/java/xws/team16/carservice/controller/GearboxController.java
+++ car-service/src/main/java/xws/team16/carservice/controller/GearboxController.java
@@ -2,6 +2,9 @@ package xws.team16.carservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xws.team16.carservice.model.Gearbox;
@@ -17,5 +20,11 @@ public class GearboxController {
     @Autowired
     public GearboxController(GearboxService gearboxService) {
         this.gearboxService = gearboxService;
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getGearboxById(@PathVariable Long id) {
+        log.info("Gearbox Controller - geGearboxById(" + id + ")");
+        return gearboxService.getGearboxById_ResponseEntity(id);
     }
 }
--- car-service/src/main/java/xws/team16/carservice/service/GearboxService.java
+++ car-service/src/main/java/xws/team16/carservice/service/GearboxService.java
@@ -2,7 +2,10 @@ package xws.team16.carservice.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import xws.team16.carservice.dto.GearboxDTO;
 import xws.team16.carservice.exceptions.NotFoundException;
 import xws.team16.carservice.model.Gearbox;
 import xws.team16.carservice.repository.GearboxRepository;
@@ -17,8 +20,20 @@ public class GearboxService {
         this.gearboxRepository = gearboxRepository;
     }
 
-    public Gearbox getGearboxById(Long gearboxId) {
-        log.info("Gearbox service - getting gearbox by id");
-        return this.gearboxRepository.findById(gearboxId).orElseThrow(() -> new NotFoundException("Gearbox with given id was nto found"));
+    public Gearbox getGearboxById(Long id) {
+        log.info("Gearbox Service - getGearboxById(" + id + ")");
+        return this.gearboxRepository.findById(id).orElseThrow(() -> new NotFoundException("Gearbox with given id was nto found"));
+    }
+
+    public ResponseEntity<?> getGearboxById_ResponseEntity(Long id) {
+        log.info("Gearbox Service - getGearboxById_ResponseEntity(" + id + ")");
+
+        Gearbox gearbox = getGearboxById(id);
+
+        GearboxDTO gearboxDTO = new GearboxDTO();
+        gearboxDTO.setId(gearbox.getId());
+        gearboxDTO.setType(gearbox.getType());
+
+        return new ResponseEntity<GearboxDTO>(gearboxDTO, HttpStatus.FOUND);
     }
 }
