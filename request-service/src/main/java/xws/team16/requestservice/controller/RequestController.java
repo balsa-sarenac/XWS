63fe3a3d1679c6d9ff1655029c0bb8f21512d36e
Stefan Kovač <stefankovac42@gmail.com>
2020-06-05 18:42:28 +0200

    Created controller for canceling requests

--- request-service/src/main/java/xws/team16/requestservice/controller/RequestController.java
+++ request-service/src/main/java/xws/team16/requestservice/controller/RequestController.java
@@ -2,10 +2,12 @@ package xws.team16.requestservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import xws.team16.requestservice.dto.OccupiedDTO;
 import xws.team16.requestservice.dto.ShoppingCartDTO;
 import xws.team16.requestservice.service.RentRequestService;
 
@@ -21,4 +23,9 @@ public class RequestController {
         return this.rentRequestService.newRequests(shoppingCart);
     }
 
+    @PostMapping(value = "/occupied" , consumes = "application/json")
+    public ResponseEntity<?> occupiedRequests(@RequestBody OccupiedDTO occupiedDTO) {
+        log.info("Request controller - occupied requests");
+        return this.rentRequestService.cancelOccupiedRequests(occupiedDTO);
+    }
 }
