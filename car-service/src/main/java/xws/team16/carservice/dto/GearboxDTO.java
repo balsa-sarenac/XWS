9cd72dd56da284f79ba391203a5a94e38b6bacc7
meelosch <57445151+meelosch@users.noreply.github.com>
2020-06-05 10:14:36 +0200

    added DTO classes

---
+++ car-service/src/main/java/xws/team16/carservice/dto/GearboxDTO.java
@@ -0,0 +1,13 @@
+package xws.team16.carservice.dto;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class GearboxDTO {
+    private Long id;
+    private String name;
+}
