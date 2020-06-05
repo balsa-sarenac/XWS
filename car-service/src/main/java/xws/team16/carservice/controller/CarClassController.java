b2f9c293bdb5168815bbdef9b23c431372958b1c
meelosch <57445151+meelosch@users.noreply.github.com>
2020-06-05 14:46:50 +0200

    implemented method for getting CarClass by its id

--- car-service/src/main/java/xws/team16/carservice/controller/CarClassController.java
+++ car-service/src/main/java/xws/team16/carservice/controller/CarClassController.java
@@ -2,6 +2,9 @@ package xws.team16.carservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import xws.team16.carservice.service.CarClassService;
@@ -16,5 +19,11 @@ public class CarClassController {
     @Autowired
     public CarClassController(CarClassService carClassService) {
         this.carClassService = carClassService;
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<?> getCarClassById(@PathVariable Long id) {
+        log.info("CarClass Controller - getCarClassById(" + id + ")");
+        return carClassService.getCarClassById_ResponseEntity(id);
     }
 }
--- car-service/src/main/java/xws/team16/carservice/service/CarClassService.java
+++ car-service/src/main/java/xws/team16/carservice/service/CarClassService.java
@@ -2,7 +2,10 @@ package xws.team16.carservice.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import xws.team16.carservice.dto.CarClassDTO;
 import xws.team16.carservice.exceptions.NotFoundException;
 import xws.team16.carservice.model.CarClass;
 import xws.team16.carservice.repository.CarClassRepository;
@@ -17,8 +20,20 @@ public class CarClassService {
         this.carClassRepository = carClassRepository;
     }
 
-    public CarClass getCarClassById(Long carClassId) {
+    public CarClass getCarClassById(Long id) {
         log.info("Car class service - getting class by id");
-        return this.carClassRepository.findById(carClassId).orElseThrow(() -> new NotFoundException("Car class with given id was not found."));
+        return this.carClassRepository.findById(id).orElseThrow(() -> new NotFoundException("Car class with given id was not found."));
+    }
+
+    public ResponseEntity<?> getCarClassById_ResponseEntity(Long id) {
+        log.info("CarClass Service - getCarClassById_ResponseEntity(" + id + ")");
+
+        CarClass carClass = getCarClassById(id);
+
+        CarClassDTO carClassDTO = new CarClassDTO();
+        carClassDTO.setId(carClass.getId());
+        carClassDTO.setName(carClass.getName());
+
+        return new ResponseEntity<CarClassDTO>(carClassDTO, HttpStatus.FOUND);
     }
 }
