93045691a56993d6f6ef93ebaa547ce864cb5ef4
Stefan Kovač <stefankovac42@gmail.com>
2020-06-14 12:50:59 +0200

    Model updated for user

--- rent-a-car/src/main/java/xws/tim16/rentacar/model/User.java
+++ rent-a-car/src/main/java/xws/tim16/rentacar/model/User.java
@@ -1,16 +1,99 @@
 package xws.tim16.rentacar.model;
 
-import java.util.Set;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+import org.springframework.security.core.userdetails.UserDetails;
 
-public class User {
+import javax.persistence.*;
+import java.sql.Timestamp;
+import java.util.Collection;
+
+@Getter @Setter @SuperBuilder
+@NoArgsConstructor @AllArgsConstructor
+@Entity @Table(name = "user_table")
+public class User implements  UserDetails {
+   @Id
+   @GeneratedValue(strategy = GenerationType.IDENTITY)
+   private Long id;
+
+   @Column(nullable = false)
    private String username;
+
+   @Column(nullable = false)
    private String password;
+
+   @Column(nullable = false)
    private String firstName;
+
+   @Column(nullable = false)
    private String lastName;
-   private boolean isEnabled;
 
-   private Set<Car> cars;
-   private Set<Ad> ads;
+   @Column(nullable = false)
+   private boolean enabled;
 
+   @Column(name = "isAdmin")
+   private boolean isAdmin;
+
+   @Column(name = "companyName")
+   private String companyName;
+
+   @Column(name = "address")
+   private String address;
+
+   @Column(name = "businessID")
+   private String businessID;
+
+   @Column
+   private Timestamp lastPasswordResetDate;
+
+   @ManyToMany(fetch = FetchType.EAGER)
+   @JoinTable(name = "users_roles",
+           joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
+           inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
+   private Collection<Role> roles;
+
+   @Column
+   private boolean accountNonExpired;
+
+   @Column
+   private boolean accountNonLocked;
+
+   @Column
+   private boolean credentialsNonExpired;
+
+   @Override
+   public Collection<Role> getAuthorities() {
+      return roles;
+   }
+
+   @Override
+   public String getPassword() {
+      return password;
+   }
+
+   @Override
+   public String getUsername() {
+      return username;
+   }
+
+   @Override
+   public boolean isAccountNonExpired() {
+      return true;
+   }
+
+   @Override
+   public boolean isAccountNonLocked() {
+      return true;
+   }
+
+   @Override
+   public boolean isCredentialsNonExpired() {
+      return true;
+   }
+
+   @Override
+   public boolean isEnabled() {
+      return enabled;
+   }
 
 }
