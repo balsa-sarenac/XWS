296f208e51cf76f64a3bf703f2691a54aa784b9b
Stefan Kovač <stefankovac42@gmail.com>
2020-06-14 12:50:35 +0200

    Added database
    Added database and  added to script roles

---
+++ rent-a-car/src/main/resources/data.sql
@@ -0,0 +1,5 @@
+-- ROLES
+insert into role (name, id) values ('ROLE_ADMIN', 1);
+insert into role (name, id) values ('ROLE_REGISTERED_USER', 2);
+insert into role (name, id) values ('ROLE_AGENT', 3);
+insert into role (name, id) values ('ROLE_USER', 4);
