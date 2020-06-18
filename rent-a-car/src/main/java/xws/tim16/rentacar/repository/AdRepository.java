4174b3be64030b72331efbd8dd7e71edaa7a7703
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-06-18 18:07:38 +0200

    Created needed repositories

---
+++ rent-a-car/src/main/java/xws/tim16/rentacar/repository/AdRepository.java
@@ -0,0 +1,9 @@
+package xws.tim16.rentacar.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import xws.tim16.rentacar.model.Ad;
+
+@Repository
+public interface AdRepository extends JpaRepository<Ad, Long> {
+}
