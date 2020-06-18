635d446a2c51124973e832cc1a2af4d64ab639c4
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-06-18 18:11:48 +0200

    Updated soap configuration and added endpoint

---
+++ car-service/src/main/java/xws/team16/carservice/endpoints/AdEndpoint.java
@@ -0,0 +1,32 @@
+package xws.team16.carservice.endpoints;
+
+import https.ftn_uns_ac_rs.ad.PostAdRequest;
+import https.ftn_uns_ac_rs.ad.PostAdResponse;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+import xws.team16.carservice.service.AdService;
+
+@Endpoint @Slf4j
+public class AdEndpoint {
+    private static final String NAMESPACE_URI = "https://ftn.uns.ac.rs/ad";
+
+    private AdService adService;
+
+    @Autowired
+    public AdEndpoint(AdService adService) {
+        this.adService = adService;
+    }
+
+    @PayloadRoot(namespace = NAMESPACE_URI, localPart = "PostAdRequest")
+    @ResponsePayload
+    public PostAdResponse newAd(@RequestPayload PostAdRequest adRequest) {
+        log.info("Ad endpoint - post new ad");
+        PostAdResponse adResponse = new PostAdResponse();
+        adResponse.setAdResponse(this.adService.newAd(adRequest));
+        return adResponse;
+    }
+}
