214202b7546768177e09b811d93285cee8a23d51
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-06-19 23:22:41 +0200

    Created functions for active and past requests

--- request-service/src/main/java/xws/team16/requestservice/repository/RentRequestRepository.java
+++ request-service/src/main/java/xws/team16/requestservice/repository/RentRequestRepository.java
@@ -1,8 +1,10 @@
 package xws.team16.requestservice.repository;
 
+import org.joda.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import xws.team16.requestservice.model.RentRequest;
+import xws.team16.requestservice.model.RequestStatus;
 import xws.team16.requestservice.model.User;
 
 import java.util.List;
@@ -13,4 +15,8 @@ public interface RentRequestRepository extends JpaRepository<RentRequest, Long> 
     List<RentRequest> findByAdId(Long id);
 
     List<RentRequest> findByUser(User user);
+
+    List<RentRequest> findByUserAndStatus(User user, RequestStatus status);
+
+    List<RentRequest> findByUserAndStatusAndReturnDateAfter(User user, RequestStatus status, LocalDate returnDate);
 }
