c0c69e481eb750f3b58583abaca5e0d957bb98a9
Stefan Kovač <stefankovac42@gmail.com>
2020-06-21 23:21:28 +0200

    added methods  for post and created controller for occupied

---
+++ rent-a-car/src/main/java/xws/tim16/rentacar/controller/OccupiedController.java
@@ -0,0 +1,37 @@
+package xws.tim16.rentacar.controller;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import xws.tim16.rentacar.dto.OccupiedDTO;
+import xws.tim16.rentacar.service.OccupiedService;
+
+
+@CrossOrigin(origins = "*")
+@RestController @Slf4j
+@RequestMapping(value = "/occupied")
+public class OccupiedController {
+    private OccupiedService occupiedService;
+
+    @Autowired
+    public OccupiedController(OccupiedService occupiedService) {
+        this.occupiedService = occupiedService;
+    }
+
+    /**
+     * Creates Occupied for specific car
+     */
+    @PostMapping
+    public ResponseEntity<?> addNew(@RequestBody OccupiedDTO occupiedDTO) {
+        log.info("Occupied controller - new occupied");
+        return this.occupiedService.newOccupied(occupiedDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<?> getNew(@PathVariable Long id ) {
+        log.info("Occupied controller - get occupied");
+        return this.occupiedService.getOccupied(id);
+    }
+
+}
