9426b62ac6598e6964610a4693d424bc3a4b8328
Stefan Kovač <stefankovac42@gmail.com>
2020-07-01 10:44:12 +0200

    added method for occupations

--- rent-a-car/src/main/java/xws/tim16/rentacar/repository/OccupiedRepository.java
+++ rent-a-car/src/main/java/xws/tim16/rentacar/repository/OccupiedRepository.java
@@ -1,9 +1,16 @@
 package xws.tim16.rentacar.repository;
 
+import org.joda.time.LocalDate;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import xws.tim16.rentacar.model.Car;
 import xws.tim16.rentacar.model.Occupied;
+
+import java.util.List;
 
 @Repository
 public interface OccupiedRepository extends JpaRepository<Occupied, Long> {
+    List<Occupied> findAllByCarAndDateToAfter(Car c, LocalDate now);
+
+    List<Occupied> findAllByCarId(Long carId);
 }
