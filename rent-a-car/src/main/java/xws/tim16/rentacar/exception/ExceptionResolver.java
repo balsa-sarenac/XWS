eba68d5be3db8d1fb3488edcb38017f694f539b2
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-07-09 20:05:04 +0200

    Added exception when user is not approved

--- rent-a-car/src/main/java/xws/tim16/rentacar/exception/ExceptionResolver.java
+++ rent-a-car/src/main/java/xws/tim16/rentacar/exception/ExceptionResolver.java
@@ -17,6 +17,12 @@ public class ExceptionResolver {
         return new ResponseEntity<String>(exception.getMessage(), headers, HttpStatus.NOT_FOUND);
     }
 
+    @ExceptionHandler(RegistrationNotApprovedException.class)
+    public ResponseEntity<?> notApproved(RegistrationNotApprovedException exception) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.TEXT_PLAIN);
+        return new ResponseEntity<String>(exception.getMessage(), headers, HttpStatus.UNAUTHORIZED);
+    }
 
     @ExceptionHandler(InvalidOperationException.class)
     public ResponseEntity<?> invalidOperation(InvalidOperationException exception) {
