c3467f7c1b97075a929eb44b7e70e5cc17d99ef5
Stefan Kovač <stefankovac42@gmail.com>
2020-07-10 17:12:16 +0200

    added role

---
+++ security-service/src/main/java/xws/team16/securityservice/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package xws.team16.securityservice.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import xws.team16.securityservice.model.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByName(String s);
+}
