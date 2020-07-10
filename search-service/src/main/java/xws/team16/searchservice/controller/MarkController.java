e4c0233405b54e02b6d447f7adc2c7ddd861dad8
Stefan Kovač <stefankovac42@gmail.com>
2020-07-10 17:10:02 +0200

    ad mark to search service

--- search-service/src/main/java/xws/team16/searchservice/controller/MarkController.java
+++ search-service/src/main/java/xws/team16/searchservice/controller/MarkController.java
@@ -2,12 +2,18 @@ package xws.team16.searchservice.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import xws.team16.searchservice.dto.MarkDTO;
 import xws.team16.searchservice.service.MarkService;
 
+import java.util.List;
+
+@CrossOrigin(value = "*")
 @RestController @Slf4j
 @RequestMapping(value="/mark")
 public class MarkController {
@@ -18,9 +24,12 @@ public class MarkController {
         this.markService = markService;
     }
 
-    @GetMapping(value="/all")
-    public ResponseEntity<?> getAll() {
-        log.info("Mark controller - get marks");
-        return this.markService.getMarks();
+    @GetMapping()
+    public ResponseEntity<?> getAll(){
+        log.info("Mark Controller - get all marks.");
+
+        List<MarkDTO> markDTOS = markService.getAll();
+
+        return new ResponseEntity<List<MarkDTO>>(markDTOS, HttpStatus.OK);
     }
 }
---
+++ search-service/src/main/java/xws/team16/searchservice/dto/MarkInfoDTO.java
@@ -0,0 +1,15 @@
+package xws.team16.searchservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MarkInfoDTO {
+    private Long id;
+    private String name;
+}
