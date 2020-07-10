06c153c8a0fef198dfda89682cf4e68c5c9eca5b
Stefan Kovač <stefankovac42@gmail.com>
2020-07-10 20:10:01 +0200

    data updated

--- request-service/src/main/resources/data.sql
+++ request-service/src/main/resources/data.sql
@@ -5,15 +5,15 @@ insert into USER_TABLE (id, username) values (3, 'bax');
 insert into USER_TABLE (id, username) values (4, 'vidak');
 insert into USER_TABLE (id, username) values (5, 'stefan');
 insert into USER_TABLE (id, username) values (6, 'miki');
-insert into USER_TABLE (id, username) values (6, 'vuja');
+insert into USER_TABLE (id, username) values (7, 'vuja');
 
 -- ADS
-insert into AD (id, user_id) values (1, 1);
-insert into AD (id, user_id) values (2, 1);
-insert into AD (id, user_id) values (3, 1);
-insert into AD (id, user_id) values (4, 1);
-insert into AD (id, user_id) values (5, 1);
-insert into AD (id, user_id) values (6, 1);
+insert into AD (id, user_id) values (1, 2);
+insert into AD (id, user_id) values (2, 2);
+insert into AD (id, user_id) values (3, 2);
+insert into AD (id, user_id) values (4, 3);
+insert into AD (id, user_id) values (5, 3);
+insert into AD (id, user_id) values (6, 3);
 
 -- RENT BUNDLES
 insert into rent_bundle (id, bundle_status) values (1, 'pending');
@@ -24,10 +24,10 @@ insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, p
 insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (2, 'pending', '2020-06-03T21:39:45.618', '2020-06-09', '2020-06-21', 'Trebinje', 2, 1, 1);
 insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (3, 'pending', '2020-06-01T21:39:45.618', '2020-06-03', '2020-06-15', 'Nevesinje', 4, 1, 1);
 insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (4, 'pending', '2020-06-01T21:39:45.618', '2020-06-06', '2020-06-26', 'Trebinje', 2, null, 1);
-insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (5, 'paid', '2020-06-01T21:39:45.618', '2020-06-03', '2020-06-30', 'Nevesinje', 5, null, 1);
-insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (6, 'paid', '2020-06-01T21:39:45.618', '2020-06-03', '2020-06-30', 'Novi Sad', 5, 2, 1);
-insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (7, 'paid', '2020-06-01T21:39:45.618', '2020-06-03', '2020-06-30', 'Novi Sad', 5, 2, 1);
+insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (5, 'paid', '2020-06-01T21:39:45.618', '2020-06-03', '2020-06-30', 'Nevesinje', 5, null, 2);
+insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (6, 'paid', '2020-06-01T21:39:45.618', '2020-06-03', '2020-06-30', 'Novi Sad', 5, 2, 2);
+insert into RENT_REQUEST (id, status, date_created, pick_up_date, return_date, pick_up_place, ad_id, bundle_id, user_id) values (7, 'paid', '2020-06-01T21:39:45.618', '2020-06-03', '2020-06-30', 'Novi Sad', 5, 2, 2);
 
-insert into bill (id, user_id, price, paid) values (1, 5, 200, false);
-insert into bill (id, user_id, price, paid) values (2, 5, 200, false);
-insert into bill (id, user_id, price, paid) values (3, 5, 200, false);
+insert into bill ( user_id, price, paid) values ( 5, 250, false);
+insert into bill ( user_id, price, paid) values ( 5, 230, false);
+insert into bill ( user_id, price, paid) values ( 5, 220, false);
