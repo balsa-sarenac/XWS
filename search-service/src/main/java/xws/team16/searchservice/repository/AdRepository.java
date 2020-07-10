03cd7a1ab0d00690b08fe9f63833fe72abe636ce
Stefan Kovač <stefankovac42@gmail.com>
2020-07-10 17:07:59 +0200

    added ad functions in serach service

--- search-service/src/main/java/xws/team16/searchservice/repository/AdRepository.java
+++ search-service/src/main/java/xws/team16/searchservice/repository/AdRepository.java
@@ -9,6 +9,8 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.stereotype.Repository;
 import xws.team16.searchservice.model.Ad;
 
+import java.util.List;
+
 @Repository
 @EnableJpaRepositories
 public interface AdRepository extends JpaRepository<Ad, Long> {
@@ -17,4 +19,6 @@ public interface AdRepository extends JpaRepository<Ad, Long> {
             "(ad.car.carClass.id = (?6) or (?6) is null) and (ad.car.fuel.id = (?7) or (?7) is null) and (ad.car.gearbox.id = (?8) or (?8) is null) and (ad.priceList.perDay >= (?9) and ad.priceList.perDay <= (?10)) and" +
             "(ad.car.kilometrage >=(?11) and ad.car.kilometrage <= (?12)) and (ad.allowedKilometrage <= (?13) or (?13)=0 or ad.allowedKilometrage = 0) and (ad.car.numberOfChildSeats = (?15) or (?15)=0) and (ad.cdwAvailable = (?14) or (?14) = false)" )
     Page<Ad> searchAds(String pickUpPlace, DateTime fromDate, DateTime toDate, Long modelId, Long markId, Long carClassId, Long fuelId, Long gearboxId, Double priceFrom, Double priceTo, Double kilomterageFrom, Double kilometrageTo, Double kilometrageDrive, Boolean cdw, Integer numberOfChildSeats, Pageable pageable);
+
+    List<Ad> findAllByToDateAfter(DateTime minusDays);
 }
