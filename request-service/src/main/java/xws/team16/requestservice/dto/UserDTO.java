47d7caa855b461cf3767d2f69ac5a89e3bb610ea
Stefan Kovač <stefankovac42@gmail.com>
2020-07-10 17:07:06 +0200

    added function to update user when is created in security service

---
+++ request-service/src/main/java/xws/team16/requestservice/dto/UserDTO.java
@@ -0,0 +1,18 @@
+package xws.team16.requestservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserDTO {
+    private Long id;
+    private String username;
+}
