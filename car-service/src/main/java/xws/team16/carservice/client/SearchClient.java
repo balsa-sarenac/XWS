266fd4fdb839e4bd02f7578f2bd86a9646faa1ec
Stefan Kovač <stefankovac42@gmail.com>
2020-07-11 09:41:11 +0200

    added feign client

--- car-service/src/main/java/xws/team16/carservice/client/SearchClient.java
+++ car-service/src/main/java/xws/team16/carservice/client/SearchClient.java
@@ -2,8 +2,11 @@ package xws.team16.carservice.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import xws.team16.carservice.dto.PriceListDTO;
 
 import java.util.List;
 
@@ -12,4 +15,10 @@ public interface SearchClient {
 
     @PostMapping(value = "/ad/remove")
     Void removeAds(@RequestBody List<Long> ads);
+
+    @PostMapping(value = "/priceList")
+    Void createPriceList(@RequestBody PriceListDTO priceListDTO);
+
+    @PutMapping( value = "/priceList", consumes = "application/json")
+    public ResponseEntity<?> editPriceList(@RequestBody PriceListDTO priceListDTO);
 }
