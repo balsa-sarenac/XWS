30d23b9340121394a1b2c93b4a06b8dd81caa59e
baxbaxbax <34557616+baxbaxbax@users.noreply.github.com>
2020-07-12 07:57:52 +0200

    Fixed chat hardcoded user id

--- admin-service/src/main/java/xws/team16/adminservice/service/UserService.java
+++ admin-service/src/main/java/xws/team16/adminservice/service/UserService.java
@@ -2,19 +2,27 @@ package xws.team16.adminservice.service;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Service;
+import xws.team16.adminservice.client.SecurityClient;
+import xws.team16.adminservice.dto.UserByUsernameDTO;
 import xws.team16.adminservice.exception.NotFoundException;
 import xws.team16.adminservice.model.User;
 import xws.team16.adminservice.repository.UserRepository;
+
+import java.util.Objects;
 
 @Service @Slf4j
 public class UserService {
 
     private UserRepository userRepository;
+    private SecurityClient securityClient;
 
     @Autowired
-    public UserService(UserRepository userRepository) {
+    public UserService(UserRepository userRepository, SecurityClient securityClient) {
         this.userRepository = userRepository;
+        this.securityClient = securityClient;
     }
 
     public User getUserById(Long userId) {
@@ -30,6 +38,14 @@ public class UserService {
         log.info("Get user from username/auth");
         // get username from auth and send request to SecurityService to give you his id
         // or add username field to user class and find it directly here
-        return getUserById(1L);
+        User user = null;
+        try {
+            ResponseEntity<UserByUsernameDTO> responseEntity = this.securityClient.getUser(
+                    SecurityContextHolder.getContext().getAuthentication().getName());
+            user = this.getUserById(Objects.requireNonNull(responseEntity.getBody()).getId());
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
     }
 }
